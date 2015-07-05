e1162d2ce03934197b6c55670af560a429203858
Vladimir Hisoka <vaxiktru@yandex.ru>
2015-07-05 20:06:00 +0300

    added merge, but work bad

--- regionSegmentation.py
+++ regionSegmentation.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageColor
-import random, sys, math
+import random, sys, math, pdb
 
 
 
@@ -21,9 +21,10 @@ class ImageWorker:
 
 		self.regionCount = 0
 		self.mode = int(neighborhood)
-		self.threshold = int(threshold) # граница
+		self.threshold = int(threshold) 
 		self.stack = []
 		self.ThresholdDict = {}
+		self.region_amount ={}
 
 	def howMuch(self):
 		print(self.regionCount)
@@ -40,9 +41,11 @@ class ImageWorker:
 		# определяем новую границу для каждой яркости. Расположение не учитывается
 		for key in self.ThresholdDict:
 			if self.threshold - self.ThresholdDict[key]/2 <=0: # пока такая формула
-				self.ThresholdDict[key] = 1
+				#self.ThresholdDict[key] = 1
+				self.ThresholdDict[key] = self.threshold
 			else:
-				self.ThresholdDict[key] = math.ceil(self.threshold - self.ThresholdDict[key]/2)
+				self.ThresholdDict[key] = self.threshold
+				#self.ThresholdDict[key] = math.ceil(self.threshold - self.ThresholdDict[key]/2)
 
 	# def printThresh(self):
 	# 	for key in self.ThresholdDict:
@@ -61,57 +64,110 @@ class ImageWorker:
 				self.grayMatrix[i][j] = (S, 0)
 		return self.grayMatrix
 
-	def findNewRegion(self):
+	def startRegionGrow(self):
 		for i in range(self.height):
 			for j in range(self.width):
-				if self.grayMatrix[j][i][1] != 0:
-					pass
-				else:
-					self.regionCount+=1
-					self.createRegion(j, i)
+				self.checkConnectivity(j, i)
 
-	def createRegion(self, coord1, coord2):
+	def createNewRegion(self, coord1, coord2):
+		self.regionCount+=1
 		self.grayMatrix[coord1][coord2] = (self.grayMatrix[coord1][coord2][0], self.regionCount)
-		self.checkConnectivity(coord1, coord2)
-		while len(self.stack) != 0:
-			elem = self.stack.pop()
-			self.checkConnectivity(elem[0], elem[1])			
+		self.region_amount[self.regionCount] = 1
+		
 
 	def checkConnectivity(self, coord1, coord2):
 		
-		count = 0
-
-		#valueOfArea = self.criteria(coord1, coord2)
+		deltaDict = {}
 
 		if self.mode == 4:
 			self.orient = self.orient[:2]
 		for item in self.orient:
 			ix = item[0]
 			iy = item[1]
-			if coord1+ix < 0 or coord1+ix > self.width-1 or coord2+iy < 0 or self.grayMatrix[coord1+ix][coord2+iy][1]==self.regionCount:
+			if coord1+ix < 0 or coord1+ix > self.width-1 or coord2+iy < 0:
 				pass
 			else:
-				if self.grayMatrix[coord1+ix][coord2+iy][1] == 0 and abs(self.grayMatrix[coord1+ix][coord2+iy][0] - self.grayMatrix[coord1][coord2][0]) < self.threshold:
-					self.grayMatrix[coord1+ix][coord2+iy] = (self.grayMatrix[coord1+ix][coord2+iy][0], self.regionCount)
-					self.stack.append((coord1+ix, coord2+iy))
+				delta = abs(self.grayMatrix[coord1+ix][coord2+iy][0]-self.grayMatrix[coord1][coord2][0])
+				deltaDict[item] = delta
 
-	def criteria(self, coord1, coord2):
-		val = 0
-		delit = 1
-		# if self.mode == "four":
-		# 	self.orient = self.orient[:4]
+		if len(deltaDict) == 0:
+			self.createNewRegion(coord1, coord2)
+			return
 
-		for item in self.orient:
+		minkey = min(deltaDict, key=deltaDict.get) #выбираем ключ с наименьшей дельтой
+		ix = minkey[0]
+		iy = minkey[1]
+		t1 = self.ThresholdDict[self.grayMatrix[coord1][coord2][0]]
+		t2 = self.ThresholdDict[self.grayMatrix[coord1+ix][coord2+iy][0]]
+		
+		if deltaDict[minkey] <= min(t1, t2):	
+			reg = self.grayMatrix[coord1+ix][coord2+iy][1]
+			self.grayMatrix[coord1][coord2] = (self.grayMatrix[coord1][coord2][0], reg)
+			self.region_amount[reg]+=1
+
+			del deltaDict[minkey]
+			self.checkRegions(deltaDict, (coord1, coord2))
+
+		else:
+			self.createNewRegion(coord1, coord2)
+
+	def mergeRegions(self, mergePoint, basePoint):
+		m = self.grayMatrix[mergePoint[0]][mergePoint[1]][1]
+		s = self.grayMatrix[basePoint[0]][basePoint[1]][1]
+		main = mergePoint
+		second = basePoint
+
+		if self.region_amount[m] < self.region_amount[s]:
+			main, second = second, main
+
+		regTo = self.grayMatrix[main[0]][main[1]][1]
+		regFrom = self.grayMatrix[second[0]][second[1]][1]
+
+		if regTo == regFrom:
+			return
+
+		self.stack.append(second)
+		self.actualMerge(second, regFrom, regTo)
+
+	
+	def actualMerge(self, nextPoint, regFrom, regTo):
+		coord1 = nextPoint[0]
+		coord2 = nextPoint[1]
+		self.grayMatrix[coord1][coord2] = (self.grayMatrix[coord1][coord2][0], regTo)
+		self.addPoint(nextPoint, regFrom, regTo)
+		while len(self.stack) != 0:
+
+			#pdb.set_trace()
+			elem = self.stack.pop()
+			self.addPoint(elem, regFrom, regTo)			
+
+	def addPoint(self, nextPoint, regFrom, regTo):
+		radius = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0)]
+		coord1 = nextPoint[0]
+		coord2 = nextPoint[1]
+		
+		for item in radius:
 			ix = item[0]
 			iy = item[1]
-			if coord1+ix < 0 or coord1+ix > self.width-1 or coord2+iy < 0 or coord2+iy > self.height-1:
+			if coord1+ix < 0 or coord1+ix > self.width-1 or coord2+iy < 0 or self.grayMatrix[coord1+ix][coord2+iy][1]!= regFrom:
 				pass
 			else:
-				if self.grayMatrix[coord1+ix][coord2+iy][1] == self.grayMatrix[coord1][coord2][1]:
-					val+= self.grayMatrix[coord1+ix][coord2+iy][0]
-					delit+=1
+				self.grayMatrix[coord1+ix][coord2+iy] = (self.grayMatrix[coord1+ix][coord2+iy][0], regTo)
+				self.stack.append((coord1+ix, coord2+iy))
 
-		return float(val/delit)
+			
+
+	def checkRegions(self, ndict, basePoint):
+		coord1 = basePoint[0]
+		coord2 = basePoint[1]
+		for key in ndict:
+			ix = key[0]
+			iy = key[1]
+			t1 = self.ThresholdDict[self.grayMatrix[coord1][coord2][0]]
+			t2 = self.ThresholdDict[self.grayMatrix[coord1+ix][coord2+iy][0]]
+			if abs(self.grayMatrix[coord1+ix][coord2+iy][0]-self.grayMatrix[coord1][coord2][0]) < min(t1, t2):
+				self.mergeRegions((coord1+ix, coord2+iy), basePoint)
+	
 
 	def generateColors(self, count):
 		AllColors = [x for x in ImageColor.colormap]
@@ -125,7 +181,6 @@ class ImageWorker:
 		regColors = [ ImageColor.getrgb(  colors[ int(random.random() * len(colors)) ] )  for x in range(self.regionCount)]
 		for i in range(self.width):
 			for j in range(self.height):
-				#if self.grayMatrix[i][j][0]> 20:
 				reg = self.grayMatrix[i][j][1]
 				self.draw.point((i,j), regColors[reg-1])			
 					
@@ -142,7 +197,7 @@ class ImageWorker:
 		self.makeGray()
 		self.defineThresholds()
 		# self.printThresh()
-		self.findNewRegion()
+		self.startRegionGrow()
 		self.howMuch()
 		self.drawImage()
 		self.image.show()
